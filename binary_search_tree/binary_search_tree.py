d459227ddc645afdc27bfdd2db57253931b7efb1
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-12 17:29:55 -0400

    Implemented insert again, but recursively this time.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,17 +5,25 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    while self:
-      if value < self.value and self.left is None:
-        self.left = BinarySearchTree(value)
-        break
-      elif value < self.value and self.left is not None:
-        self = self.left
-      elif value >= self.value and self.right is None:
-        self.right = BinarySearchTree(value)
-        break
-      else:
-        self = self.right
+    # while self:
+    #   if value < self.value and self.left is None:
+    #     self.left = BinarySearchTree(value)
+    #     break
+    #   elif value < self.value and self.left is not None:
+    #     self = self.left
+    #   elif value >= self.value and self.right is None:
+    #     self.right = BinarySearchTree(value)
+    #     break
+    #   else:
+    #     self = self.right
+    if value < self.value and self.left is None:
+      self.left = BinarySearchTree(value)
+    elif value < self.value and self.left is not None:
+      self.left.insert(value)
+    elif value >= self.value and self.right is None:
+      self.right = BinarySearchTree(value)
+    else:
+      self.right.insert(value)
 
   def contains(self, target):
     pass
